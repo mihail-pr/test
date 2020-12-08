8a4e23ece448d71481a6fb925c06513c76940f43
Mihail <52570430+mihail-pr@users.noreply.github.com>
2020-12-08 18:16:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,17 @@ if ($action == 'index') {
 } elseif ($action == 'save'){
     save();
 }
+elseif ($action == 'save_name'){
+    save_name();
+}
+elseif ($action == 'upload'){
+    upload();
+}
+elseif ($action == 'delete'){
+    delete();
+}
+
+
 
 // Functions
 
@@ -33,15 +44,35 @@ function view()
     $_GET['path'];
     $content = file_get_contents($_GET['file']);
     $file=$_GET['file'];
-
+    $name = basename($_GET['file']);
 
     $content = <<<GVS
  <h1>View Page</h1>
 
+<form method="POST" action="/?action=save_name&file=${file}">
+ <textarea name="name">${name}</textarea><br>
+<input type=submit name="edit" value="Сохранить">
+</form>
+<br>
+<br>
+
+
 <form method="post" action="/?action=save&file=${file}">
-<textarea name="edit_text">${content}</textarea>
+<textarea rows="10" cols="20"  name="edit_text">${content}</textarea><br>
 <input type="submit" name="edit_file">
 </form>
+<form method="get" action="/">
+<button>back</button>
+</form>
+
+
+<form method="post" action="/?action=delete&file=${file}">
+        <input type="submit" name="delete"
+                value="delete"/>
+    </form>
+
+
+
 GVS;
     render_layout($content);
 }
@@ -66,14 +97,58 @@ function save(){
         $handle = fopen($file_name, 'r+');
         fwrite($handle, $write_text);
         fclose($handle);
-
     }
 
+}
+function save_name(){
+    $oldname = $_GET['file'];
+    $newname = $_POST['name'];
 
+    rename($oldname,'uploads/'.$newname);
+    header('Location: /');
 
 }
 
 
+function upload(){
+    if (isset($_POST['uploadBtn']) && $_POST['uploadBtn'] == 'Upload')
+    {
+        if (isset($_FILES['uploadedFile']) && $_FILES['uploadedFile']['error'] === UPLOAD_ERR_OK)
+        {
+
+            $fileTmpPath = $_FILES['uploadedFile']['tmp_name'];
+            $fileName = $_FILES['uploadedFile']['name'];
+            $fileSize = $_FILES['uploadedFile']['size'];
+            $fileType = $_FILES['uploadedFile']['type'];
+            $fileNameCmps = explode(".", $fileName);
+            $fileExtension = strtolower(end($fileNameCmps));
+
+
+
+            $dest_path = 'uploads/';
+
+            move_uploaded_file($fileTmpPath, $dest_path.$fileName);
+            header('Location: /');
+        }
+    }
+}
+
+function delete(){
+    $path=$_GET['file'];
+
+    rmRec($path);
+
+    function rmRec($path) {
+        if (is_file($path)) return unlink($path);
+        if (is_dir($path)) {
+            foreach(scandir($path) as $p) if (($p!='.') && ($p!='..'))
+                rmRec($path.DIRECTORY_SEPARATOR.$p);
+            return rmdir($path);
+        }
+        return false;
+    }
+    header('Location: /');
+}
 function render(){
     $dir  = 'uploads';
     $allFiles = scandir($dir);
@@ -98,6 +173,16 @@ function render(){
                     ;}?>
         </tbody>
     </table>
+
+    <form method="POST" action="/?action=upload" enctype="multipart/form-data">
+        <div>
+            <span>Upload a File:</span>
+            <input type="file" name="uploadedFile" />
+        </div>
+
+        <input type="submit" name="uploadBtn" value="Upload" />
+    </form>
+
     <?php
 }
 
